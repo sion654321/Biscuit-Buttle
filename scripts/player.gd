e7526a21ae119423db5761420d26dd8a2463f178
sion654321 <sionkatz07@gmail.com>
2025-08-27 16:26:36 +0300

    עשיתי תזוזה בסיסית לשחקן

---
+++ scripts/player.gd
@@ -0,0 +1,40 @@
+extends CharacterBody2D
+
+signal health_changed(new_health)
+const SPEED = 200.0
+const JUMP_VELOCITY = -350.0
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
+
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+	
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	
+	
+	var direction := Input.get_axis("move left", "move right")
+	
+	if direction > 0:
+		animated_sprite_2d.flip_h = false
+	elif direction < 0:
+		animated_sprite_2d.flip_h = true
+	
+	if is_on_floor():
+		if direction == 0: 
+			animated_sprite_2d.play("idle")
+		else:
+			animated_sprite_2d.play("run")
+	else:
+		animated_sprite_2d.play("jump")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
