7a6dbbb918006158d5d6a05aa7bdface2bc84c75
sion654321 <sionkatz07@gmail.com>
2025-08-30 18:05:37 +0300

    i aded a dash and the tilset

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,8 +1,13 @@
 extends CharacterBody2D
 
 const SPEED = 200.0
-const JUMP_VELOCITY = -350.0
+const JUMP_VELOCITY = -430.0
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+const DASH_SPEED = 750
+var dashing = false
+@onready var dash_time: Timer = $"dash time"
+@onready var dash_again_time: Timer = $"dash again time"
+var can_dash = true
 
 
 
@@ -12,9 +17,15 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 	
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
+	
+	
+	if Input.is_action_just_pressed("dash") and can_dash:
+		dashing = true
+		dash_time.start()
+		can_dash = false
+		dash_again_time.start()
 	
 	
 	var direction := Input.get_axis("move left", "move right")
@@ -32,8 +43,22 @@ func _physics_process(delta: float) -> void:
 	else:
 		animated_sprite_2d.play("jump")
 	if direction:
-		velocity.x = direction * SPEED
+		if dashing:
+			velocity.x = direction * DASH_SPEED
+		else:
+			velocity.x = direction * SPEED
+
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+
+func _on_dash_time_timeout() -> void:
+	dashing = false
+	dash_time.stop()
+
+
+func _on_dash_again_time_timeout() -> void:
+	can_dash = true
+	dash_again_time.stop()
